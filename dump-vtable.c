11f9e0e96b0db0a25f85c37fcb9cb26fdd8824af
Andrey Ponomarenko <andrewponomarenko@yandex.ru>
2018-02-23 22:38:23 +0300

    Fixed README. Fixed license to LGPL 2.1.

--- dump-vtable.c
+++ dump-vtable.c
@@ -2,7 +2,7 @@
  * Vtable-Dumper 1.2
  * A tool to list content of virtual tables in a shared library
  * 
- * Copyright (C) 2013-2016 Andrey Ponomarenko's ABI laboratory
+ * Copyright (C) 2013-2018 Andrey Ponomarenko's ABI laboratory
  * 
  * Written by Andrey Ponomarenko
  * 
@@ -16,18 +16,20 @@
  *  libdl
  *  libstdc++
  * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License or the GNU Lesser
- * General Public License as published by the Free Software Foundation.
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
+ * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * and the GNU Lesser General Public License along with this program.
- * If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301 USA
  *
  */
 
@@ -417,8 +419,8 @@ void print_Usage()
 {
     printf("Vtable-Dumper %s\n", TOOL_VERSION);
     printf("A tool to list content of virtual tables in a shared library\n");
-    printf("Copyright (C) 2013-2016 Andrey Ponomarenko's ABI laboratory\n");
-    printf("License: GNU LGPL or GNU GPL\n\n");
+    printf("Copyright (C) 2013-2018 Andrey Ponomarenko's ABI laboratory\n");
+    printf("License: GNU LGPL 2.1\n\n");
     
     printf("Usage: vtable-dumper [options] object\n");
     printf("Example: vtable-dumper /usr/lib64/libstdc++.so.6\n");
